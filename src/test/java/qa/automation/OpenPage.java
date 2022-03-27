28a4a9af32bb0d80cda03b202b041dbc3c791f18
Iva Mladzhova <iva_mladjova0031@abv.bg>
2022-03-27 08:54:57 +0300

    Initial commit SEND

---
+++ src/test/java/qa/automation/OpenPage.java
@@ -0,0 +1,33 @@
+package qa.automation;
+
+import base.TestUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class OpenPage extends TestUtil {
+
+    @Test
+    public void successfulLoginTest(){
+        //driver.get("https://www.saucedemo.com/");
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        username.click();
+        username.sendKeys("standard_user");
+
+        WebElement password = driver.findElement(By.xpath("(//input[@class='input_error form_input'])[2]"));
+        password.click();
+        password.sendKeys("secret_sauce");
+
+        WebElement LoginBtn = driver.findElement(By.cssSelector("[value=Login]"));
+        LoginBtn.click();
+
+        WebElement userAllPagesButton = driver.findElement(By.id("react-burger-menu-btn"));
+        Assert.assertTrue(userAllPagesButton.isDisplayed());
+
+    }
+
+
+
+}
---
+++ src/test/java/qa/automation/UnsuccessfulLogin.java
@@ -0,0 +1,58 @@
+package qa.automation;
+
+import base.TestUtil;
+import com.opencsv.exceptions.CsvException;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utils.CsvHelper;
+
+import java.io.IOException;
+
+public class UnsuccessfulLogin extends TestUtil {
+//    private WebDriver driver;
+
+
+//    @BeforeTest
+//    public void initializeDriver(){
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//    }
+
+//    @AfterTest
+//    public void tearDown(){
+//        driver.quit();
+//    }
+
+    @DataProvider(name = "UnsuccessfulLogin.csv")
+    public static Object[][] readUsersFromCsvFile() throws IOException, CsvException {
+        return CsvHelper.readCsvFile("src/test/resources/UnsuccessfulLogin.csv");
+    }
+
+    @Test (dataProvider = "UnsuccessfulLogin.csv")
+    public void UnsuccessfulLoginTest(String userName, String password) {
+        //driver.get("https://www.saucedemo.com/");
+
+        WebElement username = driver.findElement(By.id("user-name"));
+        username.click();
+        username.sendKeys(userName);
+
+        WebElement passwordInput = driver.findElement(By.xpath("(//input[@class='input_error form_input'])[2]"));
+        passwordInput.click();
+        passwordInput.sendKeys(password);
+
+        WebElement LoginBtn = driver.findElement(By.cssSelector("[value=Login]"));
+        LoginBtn.click();
+
+        WebElement errorLoginLabel = driver.findElement(By.xpath("//*[text()='Epic sadface: Username and password do not match any user in this service']"));
+        Assert.assertTrue(errorLoginLabel.isDisplayed());
+    }
+
+}
